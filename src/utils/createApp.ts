0a0e068460522e94dc73dd00464793cf27401e5f
subhanyousaf <subhanyousaf2003@gmail.com>
2024-02-23 09:11:42 -0800

    :zap: increase size

--- src/utils/createApp.ts
+++ src/utils/createApp.ts
@@ -10,8 +10,8 @@ require("../strategies/local");
 
 export function createApp() {
   const app = express();
-  app.use(express.json({ limit: "50mb" }));
-  app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+  app.use(express.json({ limit: "200mb" }));
+  app.use(express.urlencoded({ extended: true, limit:  "200mb" }));
 
   app.use(
     cors({
